3fcc65a638a899c352c38b7248e1b7b7279750a2
Daniel Rangel <rangeldor@gmail.com>
2022-10-31 15:55:34 -0300

    Adiciona fontawesome e tema do vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,42 @@
-// Styles
 import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import 'vuetify/styles'
-
-// Vuetify
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+import { mdi } from 'vuetify/iconsets/mdi'
+import { md } from 'vuetify/iconsets/md'
+import { mdi as mdiSvg } from 'vuetify/iconsets/mdi-svg'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+// Misc
+import { loadFonts } from './webfontloader'
+loadFonts()
+
+export default createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+      mdi,
+      md,
+      mdiSvg
+    }
+  },
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          background: '#F7F7F7',
+          primary: '#43807a',
+          secondary: '#5CBBF6',
+          tertiary: '#E57373',
+          accent: '#005CAF'
+        }
+      }
+    }
+  }
+})
