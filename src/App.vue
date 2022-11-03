afbf842c432f6e842e420bc7020e1ab607f9063d
Daniel Rangel <rangeldor@gmail.com>
2022-11-03 15:24:17 -0300

    Adiciona roteamento global

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,58 @@
 <template>
   <v-app>
     <v-main>
-      <h1 class="text-primary">
-      Todo List - JSON Placeholder
-    </h1>
-    <TodoList />
+      <router-view v-slot="{ Component }" >
+        <transition name="fade" >
+          <component :is="Component" />
+        </transition>
+      </router-view>
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import TodoList from './components/todo/TodoList.vue'
-
-export default defineComponent({
-  name: 'App',
-
-  components: {
-    HelloWorld,
-    TodoList
-},
-
-  data () {
-    return {
-      //
-    }
-  },
-})
-</script>
+<style lang="scss">
+html {
+  overflow-y: auto !important;
+}
+html,
+body {
+  height: 100% !important;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.scale-enter-active,
+.scale-leave-active {
+  transition: all 0.4s ease;
+}
+.scale-enter-from,
+.scale-leave-to {
+  opacity: 0;
+  transform: scale(0.9);
+}
+input:-webkit-autofill {
+  box-shadow: 0 0 0 1000px #fff inset !important;
+  -webkit-box-shadow: 0 0 0 1000px #fff inset !important;
+}
+::-webkit-scrollbar {
+  width: 12px;
+}
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #ccc;
+}
+</style>
