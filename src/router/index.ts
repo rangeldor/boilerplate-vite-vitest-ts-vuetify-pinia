d673ccb1e82ca071aafbe7325f2cad7ac7eaf7d8
Daniel Rangel <rangeldor@gmail.com>
2022-11-03 15:24:00 -0300

    Cria arquivo de rota

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { RouteName, RoutePath } from '@/enums/route'
+const Home = () => import(/* webpackChunkName: "Home" */ '@/pages/Home.vue')
+
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: RoutePath.Home,
+    name: RouteName.Home,
+    component: Home
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  scrollBehavior () {
+    return {
+      top: 0,
+      behavior: 'smooth'
+    }
+  }
+})
+
+export default router
