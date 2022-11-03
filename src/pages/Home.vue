f571deffd5d16c3bb63799343fdcb9a1e5830c12
Daniel Rangel <rangeldor@gmail.com>
2022-11-03 15:24:37 -0300

    Cria Homepage para adição no teste da rota

---
+++ src/pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <v-main>
+      <h1 class="text-primary">
+      Todo List - JSON Placeholder
+    </h1>
+<TodoList />
+    </v-main>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TodoList from '@/components/todo/TodoList.vue'
+
+export default defineComponent({
+  name: 'Home',
+
+  components: {
+    TodoList
+},
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
