e3329b644cedc65b54c4fc6560250103cb8276f8
Daniel Rangel <rangeldor@gmail.com>
2022-11-09 18:03:32 -0300

    Importa biblioteca animate.css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import 'animate.css'
 
 loadFonts()
 
