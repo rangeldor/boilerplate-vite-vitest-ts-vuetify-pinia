131fd666da3818cbb70b0b6f57984b0801563d8b
Daniel Rangel <rangeldor@gmail.com>
2022-11-10 16:12:10 -0300

    Ajusta layout

---
+++ src/pages/dashboard/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-primary">
+      Todo List - JSON Placeholder
+    </h1>   
+
+    <TodoList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TodoList from '@/components/todo/TodoList.vue'
+
+</script>
