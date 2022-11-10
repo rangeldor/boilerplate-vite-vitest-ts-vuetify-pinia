33d86fbaeb46f19dff80f6d0a212190dcbd29f5f
Daniel Rangel <rangeldor@gmail.com>
2022-11-10 16:11:23 -0300

    Cria componente de pagina não encontrada

---
+++ src/pages/dashboard/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Página não encontrada</h1>
+    <p>
+      Oops, Não foi possível encontrar essa página.
+      <router-link to="/">
+        Retornar a Página inicial
+      </router-link>
+    </p>
+  </div>
+</template>
