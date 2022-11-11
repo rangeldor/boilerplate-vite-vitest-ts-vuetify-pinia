9f786bdec9ce6d38f44d840e905371f92797e68d
Daniel Rangel <rangeldor@gmail.com>
2022-11-11 12:56:28 -0300

    Modifica teste e2e para funcionar

--- tests/e2e/specs/home.cy.ts
+++ tests/e2e/specs/home.cy.ts
@@ -1,6 +1,6 @@
 describe('Home', () => {
     it('visits the home root url', () => {
         cy.visit('/')
-        cy.contains('h1', 'Todo List - JSON Placeholder')
+        cy.contains('h1', 'Home page')
     })
 })
