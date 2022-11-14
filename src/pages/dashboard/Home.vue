b6691a4d8d561891094c878cd17eebbe5bf5ea45
Daniel Rangel <rangeldor@gmail.com>
2022-11-14 15:59:16 -0300

    Retira textos de exemplos

--- src/pages/dashboard/Home.vue
+++ src/pages/dashboard/Home.vue
@@ -6,7 +6,6 @@
     >
       the quick brown fox jumps over the lazy dog
     </h1>
-    <span class="font-weight-bold">tttt: </span><span>the quick brown fox jumps over the lazy dog</span>
   </div>
 </template>
 
