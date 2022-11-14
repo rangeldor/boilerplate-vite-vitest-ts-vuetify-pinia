3387379a10e5672f2b23a3feaad7da0c2a7afd9f
Daniel Rangel <rangeldor@gmail.com>
2022-11-14 15:59:34 -0300

    Adiciona composable do alerta global

---
+++ src/composables/useAlert.ts
@@ -0,0 +1,55 @@
+import { Ref, ref } from "vue"
+import { StatusType } from '@/enums/status'
+
+interface IAlertComposable {
+    active: Ref<boolean>
+    title: Ref<string>
+    description: Ref<string>
+    type: Ref<StatusType>
+    toogle(): void
+    approved(): void
+    disapproved(): void
+    error(): void
+   
+}
+
+const active = ref<boolean>(false)
+const title = ref<string>('')
+const description = ref<string>('')
+const type = ref<StatusType>(StatusType.SUCCESS)
+
+export function useAlert(): IAlertComposable {
+    const show = (titleValue: string, descriptionValue: string, typeValue: StatusType) => {
+        title.value = titleValue
+        description.value = descriptionValue
+        type.value = typeValue
+        active.value = true
+    }
+
+    const toogle = () => {
+        active.value = !active.value
+    }
+
+    const approved = () => {
+        const title = 'Aprovado com sucesso'
+        const description = 'Aprovado com sucesso'
+
+        show(title, description, StatusType.SUCCESS)
+    }
+
+    const disapproved = () => {
+        const title = 'Reprovado com sucesso'
+        const description = 'Reprovado com sucesso'
+
+        show(title, description, StatusType.SUCCESS)
+    }
+
+    const error = () => {
+        const title = 'Aconteceu algum erro'
+        const description = 'Aconteceu algum erro'
+
+        show(title, description, StatusType.WARNING)
+    }
+
+    return { active, type, title, description, toogle, approved, disapproved, error }
+}
