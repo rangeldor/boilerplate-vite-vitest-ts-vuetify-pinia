2406e16cfda48db7dfdbc74ff46eead045ace612
Daniel Rangel <rangeldor@gmail.com>
2022-11-14 14:11:44 -0300

    Retira espaço em branco e importação comentada

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import 'animate.css'
 
-
 const pinia = createPinia()
 
 createApp(App)
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,5 @@
 import '@mdi/font/css/materialdesignicons.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-// import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import { aliases, fa } from 'vuetify/iconsets/fa'
 import { mdi } from 'vuetify/iconsets/mdi'
